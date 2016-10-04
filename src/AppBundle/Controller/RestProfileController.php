02614679d0dc4abbc846823f22018216a9806f10
Chris <chris@codereviewvideos.com>
2016-10-04 12:06:19 +0100

    profile imp

--- src/AppBundle/Controller/RestProfileController.php
+++ src/AppBundle/Controller/RestProfileController.php
@@ -3,10 +3,18 @@
 namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Patch;
+use FOS\RestBundle\Controller\Annotations\Put;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\FOSUserEvents;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -35,4 +43,84 @@ class RestProfileController extends FOSRestController implements ClassResourceIn
         return $user;
     }
 
+
+    /**
+     * @param Request $request
+     *
+     * @Put("/profile/{user}")
+     *
+     * @ParamConverter("user", class="AppBundle:User")
+     */
+    public function putAction(Request $request, UserInterface $user)
+    {
+        return $this->updateProfile($request, $user, true);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @Patch("/profile/{user}")
+     *
+     * @ParamConverter("user", class="AppBundle:User")
+     */
+    public function patchAction(Request $request, UserInterface $user)
+    {
+        return $this->updateProfile($request, $user, false);
+    }
+
+
+    private function updateProfile(Request $request, UserInterface $user, $clearMissing = true)
+    {
+        $user = $this->getAction($user);
+
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->get('event_dispatcher');
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->get('fos_user.profile.form.factory');
+
+        $form = $formFactory->createForm(['csrf_protection' => false]);
+        $form->setData($user);
+
+        $form->submit($request->request->all(), $clearMissing);
+
+        if ( ! $form->isValid()) {
+            // return the form with any errors if invalid
+            return $form;
+        }
+
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->get('fos_user.user_manager');
+
+        $event = new FormEvent($form, $request);
+        $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_SUCCESS, $event);
+
+        $userManager->updateUser($user);
+
+        if (null === $response = $event->getResponse()) {
+            return $this->routeRedirectView(
+                'get_profile',
+                [ 'user' => $user->getId() ],
+                Response::HTTP_NO_CONTENT
+            );
+        }
+
+        $dispatcher->dispatch(
+            FOSUserEvents::PROFILE_EDIT_COMPLETED,
+            new FilterUserResponseEvent($user, $request, $response)
+        );
+
+        return $this->routeRedirectView(
+            'get_profile',
+            [ 'user' => $user->getId() ],
+            Response::HTTP_NO_CONTENT
+        );
+    }
 }
--- src/AppBundle/Features/profile.feature
+++ src/AppBundle/Features/profile.feature
@@ -14,7 +14,13 @@ Feature: Manage User profile data via the RESTful API
     And I am successfully logged in with username: "peter", and password: "testpass"
 
 
-    @this
+    Scenario: Cannot view a profile with a bad JWT
+      When I set header "Authorization" with value "Bearer some-bad-token"
+      And I send a "GET" request to "/profile/1"
+      Then the response code should be 401
+      And the response should not contain "invalid JWT token"
+
+
     Scenario: Can view own profile
       When I send a "GET" request to "/profile/1"
       Then the response code should be 200
@@ -26,3 +32,82 @@ Feature: Manage User profile data via the RESTful API
           "email": "peter@test.com"
         }
         """
+
+
+    Scenario: Cannot view another user's profile
+      When I send a "GET" request to "/profile/2"
+      Then the response code should be 403
+
+
+    Scenario: User must supply a valid current password when updating their Profile
+      When I send a "PUT" request to "/profile/1" with body:
+          """
+          {
+            "username": "peter",
+            "email": "new_email@test.com"
+          }
+          """
+      Then the response code should be 400
+
+
+    Scenario: User can update their profile by using PUT (replace)
+      When I send a "PUT" request to "/profile/1" with body:
+        """
+        {
+          "username": "peter",
+          "email": "new_email@test.com",
+          "current_password": "testpass"
+        }
+        """
+      Then the response code should be 204
+      And I send a "GET" request to "/profile/1"
+      And the response should contain json:
+        """
+        {
+          "id": 1,
+          "username": "peter",
+          "email": "new_email@test.com"
+        }
+        """
+
+    Scenario: User cannot update another User's profile
+      When I send a "PUT" request to "/profile/2" with body:
+          """
+          {
+            "username": "peter",
+            "email": "new_email@test.com",
+            "current_password": "testpass"
+          }
+          """
+      Then the response code should be 403
+
+
+    Scenario: User can partially update their profile by using PATCH
+      When I send a "PATCH" request to "/profile/1" with body:
+          """
+          {
+            "email": "other_email@test.com",
+            "current_password": "testpass"
+          }
+          """
+      Then the response code should be 204
+      And I send a "GET" request to "/profile/1"
+      And the response should contain json:
+          """
+          {
+            "id": 1,
+            "username": "peter",
+            "email": "other_email@test.com"
+          }
+          """
+
+
+    Scenario: User cannot update another User's profile
+      When I send a "PATCH" request to "/profile/2" with body:
+            """
+            {
+              "email": "new_email@test.com",
+              "current_password": "testpass"
+            }
+            """
+      Then the response code should be 403
