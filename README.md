230a653930c89faf47d8199bc6807c3aafea4179
Chris <chris@codereviewvideos.com>
2016-11-05 10:59:57 +0000

    JWT created listener

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ FOS User Bundle with FOS REST Bundle integration
 """"""""""""""""""""""""""""""""""""""""""""""""
 
 A work in progress
+
+
+{
+  "id": 1,
+  "username": "peter",
+  "username_canonical": "peter",
+  "email": "peter@test.com",
+  "email_canonical": "peter@test.com",
+  "enabled": true,
+  "salt": "jdo5081aybcws8ck4sgk4c0ssg4c8s0",
+  "password": "$2y$13$CVnDR/WDbTbRIOtEblcDg.UCOTu.FADIyix93W/q2xxvXMxOAsaJC",
+  "last_login": "2016-11-05T10:57:50+0000",
+  "groups": [],
+  "locked": false,
+  "expired": false,
+  "roles": [],
+  "credentials_expired": false
+}
--- src/AppBundle/Event/Listener/JWTCreatedListener.php
+++ src/AppBundle/Event/Listener/JWTCreatedListener.php
@@ -4,27 +4,21 @@ namespace AppBundle\Event\Listener;
 
 use AppBundle\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class JWTCreatedListener
 {
-    /**
-     * @var TokenStorageInterface
-     */
-    private $tokenStorage;
-
-    public function __construct(TokenStorageInterface $tokenStorage)
-    {
-        $this->tokenStorage = $tokenStorage;
-    }
-
     public function onJWTCreated(JWTCreatedEvent $event)
     {
-        /** @var $user User */
+        /**
+         * @var $user User
+         */
         $user = $event->getUser();
 
-        $payload['userId'] = $user->getId();
-        $payload['username'] = $user->getUsernameCanonical();
+        $payload = [
+            'id' => $user->getId(),
+            'username' => $user->getUsernameCanonical(),
+        ];
+
 
         $event->setData($payload);
     }
