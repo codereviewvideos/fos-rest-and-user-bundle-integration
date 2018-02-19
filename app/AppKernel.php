3ca0898cc47b67df79637cdd0e85b2fa24c517e4
chris <chris@a6software.co.uk>
2018-02-19 15:20:14 +0000

    symfony 3.4 update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,9 +31,13 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
 
-            $bundles[] = new Csa\Bundle\GuzzleBundle\CsaGuzzleBundle();
+            if ('dev' === $this->getEnvironment()) {
+                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+            }
+
+            $bundles[] = new EightPoints\Bundle\GuzzleBundle\GuzzleBundle();
         }
 
         return $bundles;
